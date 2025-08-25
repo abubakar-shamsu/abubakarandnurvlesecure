2aa2655a2d2f426d15b19210e73dfa424d3ba857
abubakarshamsuawal <abubakarshamsuawal@gmail.com>
2025-08-25 19:41:53 +0300

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,1 +1,10 @@
 
+export async function handler() {
+ return {
+  statusCode: 200,
+  body: JSON.stringify({
+   API_KEY: process.env.API_KEY,
+   currentModel: process.env.currentModel
+  })
+ };
+}
